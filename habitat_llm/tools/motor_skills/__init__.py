5f15a36a938cc60fe423e18da93d55bdbcbf3940
liyang <liyang372328@163.com>
2025-04-10 19:58:47 +0800

    add new custom skill

--- habitat_llm/tools/motor_skills/__init__.py
+++ habitat_llm/tools/motor_skills/__init__.py
@@ -17,6 +17,7 @@ from habitat_llm.tools.motor_skills.nav.oracle_nav_skill import OracleNavSkill
 # Pick
 from habitat_llm.tools.motor_skills.pick.nn_pick_skill import PickSkillPolicy
 from habitat_llm.tools.motor_skills.pick.oracle_pick_skill import OraclePickSkill
+from habitat_llm.tools.motor_skills.pick.custom_pick_skill import CustomPickSkill
 
 # Place
 from habitat_llm.tools.motor_skills.place.nn_place_skill import PlaceSkillPolicy
